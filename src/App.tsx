f3aabf644c4d08a47af4ef9cf163b5afd62b1c19
Beata Kula <beata.kula@intive.com>
2022-04-06 15:53:47 +0200

    code clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,88 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import Post from "./components/Post";
 import Loader from "./components/Loader";
-import Message, { categoryTypeString }from "./components/Message";
+import Message, { categoryTypeString } from "./components/Message";
 
 import "./App.css";
 
 type appStateType = {
-  posts: Array<typeof Post>,
-  isloaded: boolean,
-  isError: boolean,
-  headerMessageText: String,
-  messageText: String,
-  showMessage: boolean,
-  categoryMessage: categoryTypeString
-}
+    posts: Array<typeof Post>;
+    isloaded: boolean;
+    isError: boolean;
+    headerMessageText: String;
+    messageText: String;
+    showMessage: boolean;
+    categoryMessage: categoryTypeString;
+};
 
-class App extends Component <{}, appStateType> {
-  categoryMessageInfo: categoryTypeString = "info";
+class App extends Component<{}, appStateType> {
+    categoryMessageInfo: categoryTypeString = "info";
 
-  state = {
-      posts:[],
-      isloaded: false,
-      isError: false,
-      headerMessageText: "",
-      messageText: "",
-      showMessage: false,
-      categoryMessage: this.categoryMessageInfo
-  }
+    state = {
+        posts: [],
+        isloaded: false,
+        isError: false,
+        headerMessageText: "",
+        messageText: "",
+        showMessage: false,
+        categoryMessage: this.categoryMessageInfo,
+    };
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(
-        response => {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            this.setState({
-              posts: [], 
-              isloaded: false,
-              isError:true,
-              headerMessageText: "We're sorry we can't show you Blog!",
-              messageText: "An error occurred while loading data: <br/>" + response.text,
-              showMessage: true,
-              categoryMessage: "negative",
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    this.setState({
+                        posts: [],
+                        isloaded: false,
+                        isError: true,
+                        headerMessageText:
+                            "We're sorry we can't show you Blog!",
+                        messageText:
+                            "An error occurred while loading data: <br/>" +
+                            response.text,
+                        showMessage: true,
+                        categoryMessage: "negative",
+                    });
+
+                    return [];
+                }
+            })
+            .then((json) => {
+                this.setState({
+                    posts: json,
+                    isloaded: true,
+                });
             });
+    }
 
-            return [];
-          }
-        }
-      )
-      .then(json => {
-        this.setState({
-            posts: json, 
-            isloaded: true
+    render() {
+        const postsList = this.state.posts.map((post) => {
+            return <Post key={post["id"]} {...post} />;
         });
-      });
-  }
 
-  render(){
-    const postsList = this.state.posts.map((post) => {
-      return <Post key={post['id']}  post={post} />;
-    });
-
-    return (
-      <div className="App">
-          <header className="App-header">
-              <h1>Blog Beaty</h1>
-          </header>
-          <div>
-              <Loader isActive={!this.state.isloaded}/>
-              <Message
-                showMessage={this.state.showMessage}
-                category={this.state.categoryMessage}
-                headerText={this.state.headerMessageText}
-                text={this.state.messageText}
-                color="red"
-                size="large"
-              />
-              <ul className="post-list">{postsList}</ul>
-          </div>
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Blog Beaty</h1>
+                </header>
+                <div>
+                    <Loader isActive={!this.state.isloaded} />
+                    <Message
+                        showMessage={this.state.showMessage}
+                        category={this.state.categoryMessage}
+                        headerText={this.state.headerMessageText}
+                        text={this.state.messageText}
+                        color="red"
+                        size="large"
+                    />
+                    <ul className="post-list">{postsList}</ul>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 
-type PostProps  = {
-    post: {
-        userId: number,
-        id: number,
-        title: string,
-        body: string
-    }
-}
-  
+type PostProps = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
 
-const Post: React.ComponentType<PostProps>  = ({ post }) => {
+const Post: React.ComponentType<PostProps> = ({ userId, title, body }) => {
     return (
         <li className="post-element">
-            <h4>Autor: {post.userId}</h4>
+            <h4>Autor: {userId}</h4>
             <h3>
-                <b>{post.title}</b>
+                <b>{title}</b>
             </h3>
             <br />
-            {post.body}
+            {body}
         </li>
     );
 };
